d8f2702644680aa4782a577b633c07314165aabd
Ahmed Eid <a.eid@yandex.com>
2018-07-19 08:45:54 +0200

    init commit

---
+++ pages/about/index.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from "react";
+import Layout from "../../components/Layout";
+
+export default () => (
+  <Fragment>
+    <Layout title="Home">
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis,
+        iure accusantium provident minus ab eligendi voluptates maxime soluta
+        adipisci omnis cum saepe commodi quasi laboriosam ea nobis possimus
+        maiores reiciendis.
+      </div>
+    </Layout>
+    <style jsx>{``}</style>
+  </Fragment>
+);
---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import React, {Fragment} from "react"
+import Layout from '../components/Layout'
+
+
+export default () => (
+  <Fragment>
+    <Layout title="Home">
+      <div>
+        home page
+      </div>
+    </Layout>
+
+    <style jsx>{`
+    `}</style>
+  </Fragment>
+)
